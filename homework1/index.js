6f8f109e18697acb73becbef2d9085475e273819
abil11 <abil98@mail.ru>
2017-05-16 11:43:13 -0700

    Add files via upload

--- homework1/index.js
+++ homework1/index.js
@@ -1,1 +1,34 @@
 
+var http = require("http"), fs = require('fs');
+
+function serveStatic(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  console.log(__dirname + path)
+  fs.readFile(__dirname + path, function(err, data){
+      if(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Error');
+      }
+      else{
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+      }
+  });
+}
+
+http.createServer(function(req,res){
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      serveStatic(res, '/public/index.html', 'text/html');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('This is page about My site');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404:Page not found.');
+  }
+
+}).listen(process.env.PORT || 3000);
