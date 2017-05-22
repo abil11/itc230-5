4757e9e6af6fb47713d0b1c11763fa1ed8b29880
abil11 <abil98@mail.ru>
2017-05-22 15:37:21 -0700

    Add files via upload

--- homework2/index.js
+++ homework2/index.js
@@ -1,1 +1,51 @@
+'use strict'
 
+let book = require("../lib/book.js");
+
+const express = require("express");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(require("body-parser").urlencoded({extended: true}));
+
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+// send static file as response
+app.get('/', function(req,res){
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+
+app.get('/about', function(req,res){
+    res.type('text/plain');
+    res.send('About page');
+});
+
+//  GET
+app.get('/delete', function(req,res){
+    let result = book.delete(req.query.title); // delete book object
+    res.render('delete', {title: req.query.title, result: result});
+});
+
+// POST
+app.post('/get', function(req,res){
+    console.log(req.body)
+    var header = 'Searching for: ' + req.body.title + '<br>';
+    var found = book.get(req.body.title);
+    res.render("details", {title: req.body.title, result: found});
+});
+
+// 404
+app.use(function(req,res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started');
+});
