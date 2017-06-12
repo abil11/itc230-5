71717c8b47db6e69cc3599138825eb5988707777
abil11 <abil98@mail.ru>
2017-06-11 20:41:28 -0700

    Add files via upload

--- homewrok2 -actual/lib/music.js
+++ homewrok2 -actual/lib/music.js
@@ -1,1 +1,26 @@
+//list of items
+let music = [
+    {course: "rock", performer: "Oxxymiron", genre:"Rock"},
+    {course: "rnb", performer: "BOYJ", genre:"RNB"},
+    {course: "Rap", performer: "NAta", genre:"Rap"},
+];
 
+
+
+exports.get = (course) => {
+    return music.find((item) => {
+    return item.course == course;
+    });
+}
+
+exports.delete = (course) => {
+    let oldLength = music.length;
+    var newCourse = music.filter((item) => {
+        return item.course !==course;
+    })
+    music = newCourse;
+
+
+
+return { deleted: music.length !==oldLength, total: music.length}
+};
