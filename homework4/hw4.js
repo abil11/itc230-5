de989fcf809fb8e4c14c90324c17bed4cd9fc2b9
abil11 <abil98@mail.ru>
2017-06-15 23:41:21 -0700

    Add files via upload

---
+++ homework4/hw4.js
@@ -0,0 +1,7 @@
+"use strict";
+// eslint practice file for ITC 230
+var names = ['sara','joe','dave','ann', 'sdfksm','computer', 'mac'];
+let newArray = names.map((item) => {
+return item.toUpperCase();
+}) ;
+newArray;
