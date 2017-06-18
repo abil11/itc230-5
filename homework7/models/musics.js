af63e1e0d45517ede8f3f8e85ce212d8bc00a122
abil11 <abil98@mail.ru>
2017-06-18 13:40:10 -0700

    Add files via upload

--- homework7/models/musics.js
+++ homework7/models/musics.js
@@ -1,1 +1,23 @@
 
+
+//var credentials = require("../credentials");
+var mongoose = require("mongoose");
+
+
+
+  var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }  } };
+//  mongoose.connect(credentials.mongo.development.connectionString, options);
+  mongoose.connect("mongodb://zmon:12345tyu@ds127872.mlab.com:27872/abil", options);
+
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+var musicSchema = mongoose.Schema({
+  type: String,
+  band: String,
+  singer:String,
+  label:String,
+  bandmembers: Number,
+});
+module.exports = mongoose.model('Music', musicSchema);
